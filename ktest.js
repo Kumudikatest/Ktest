04cb32a0b2ba2c9bb937781c1925ddce66fe7544
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-08 10:55:11 +0530

    test

--- ktest.js
+++ ktest.js
@@ -1,28 +1,4 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-const kinesis = new AWS.Kinesis();
-
 exports.handler = async (event) => {
-
-    try {
-        let data = await kinesis.putRecord({
-            StreamName: "K",
-            Data: "d",
-            PartitionKey: "d"
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
-    try {
-        let data = await cognito_idp.listUsers({
-            UserPoolId: process.env.UserPoolId_cognitoNewpool,
-            Limit: 10
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
