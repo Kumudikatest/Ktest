ac46e11525fcd5b5f20dcb90d23f8484514ecbc6
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-10 10:41:49 +0530

    test

--- ktest.js
+++ ktest.js
@@ -1,16 +1,25 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
 
 exports.handler = function (request, response) {
-    cognito_idp.listUsers({
-        UserPoolId: "us-east-1_HdYJb7Znp",
-        Limit: 10
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
+    datastore.projects.beginTransaction({
+        projectId: process.env.GCP_PROJECT,
+        resource: {
+            transactionOptions: {
+                readWrite: {}
+            }
+        }
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "transaction": "<transaction ID>"
+        }
+        */
+    })
         .catch(err => {
-            // error handling goes here
+            console.log(err, err.stack); // an error occurred
         });
 
     response.send({ "message": "Successfully executed" });
